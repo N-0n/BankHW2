31a8d7a5e5d8d1f9780c3c096879f42dc8360a11
N-0n <noureenfatima.nen@gmail.com>
2024-12-18 23:30:14 +0500

    bank2

---
+++ script.js
@@ -0,0 +1,21 @@
+let balance=Number(document.getElementById('balance').innerText);
+
+
+function credit(){
+    let amount = parseFloat(document.getElementById('amount').value);
+    balance = balance + amount;
+    document.getElementById('balance').innerText=balance;
+    document.getElementById('amount').value = '';
+}
+
+function debit(){
+    let amount = parseFloat(document.getElementById('amount').value);
+    if (amount <= balance) {
+        balance= balance - amount;
+        document.getElementById('balance').innerText=balance;
+        document.getElementById('amount').value = '';
+
+    } else {
+        alert('Amount cannot be withdrawn, Insufficient balance!')
+    } 
+}
